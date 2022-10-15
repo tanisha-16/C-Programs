beccf6bc033620808cc3c9727c4485cf892ebb37
tanisha-16 <tanishasingh71502@gmail.com>
2022-10-15 17:41:52 +0530

    NCC program

---
+++ program_19.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+
+    int age;
+    float weight, height;
+    printf("enter your age: \n");
+    scanf("%d", &age);
+    printf("enter your weight: \n");
+    scanf("%d", &weight);
+    printf("enter your height: \n");
+    scanf("%d", &height);
+    if (age >= 18)
+    {
+        if (weight >= 60)
+        {
+            if (height >= 5.5)
+            {
+                printf("you are eligible for NCC");
+            }
+            else
+            {
+                printf("your height is not fit");
+            }
+        }
+        else
+        {
+            printf("your weight is not fit");
+        }}
+        else
+        {
+            printf("your age is below 18");
+        }
+
+        return 0;
+    }
